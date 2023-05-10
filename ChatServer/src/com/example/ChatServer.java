daa31bb52938c63c9481ffe8c204913c0ccbd0c9
Ruslana <ruslana.ru@inbox>
2023-05-10 23:13:11 +0700

    ChatServer commit one

---
+++ ChatServer/src/com/example/ChatServer.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ChatServer {
+ArrayList<Client> clients = new ArrayList<>();
+    ServerSocket serverSocket;
+ChatServer() throws IOException {
+    // создаем серверный сокет на порту 1234
+    serverSocket = new ServerSocket(1234);
+}
+
+void sendAll(String message) {
+    for (Client client: clients) {
+        client.receive(message);
+    }
+}
+public void run() throws IOException {
+    while(true) {
+        System.out.println("Waiting...");
+
+        try {
+        // ждем клиента из сети
+        Socket socket = serverSocket.accept();
+        System.out.println("Client connected!");
+
+        // создаем клиента на своей стороне
+        clients.add(new Client(socket, this));
+
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+    public static void main(String[] args) throws IOException {
+        new ChatServer().run();
+
+
+    }
+}
